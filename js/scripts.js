35cc46917b14edef052fd3e7ec3627333a0e550c
Nelson Rosales <56582884+nelson2411@users.noreply.github.com>
2024-11-18 23:14:43 +0100

    Add cover.pdf and enhance image rotation effect on hover

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,9 @@ make a image rotate on hover
 const atomImg = document.getElementById("atom")
 
 atomImg.addEventListener("mouseover", () => {
-  atomImg.style.transform = "rotate(360deg)"
+  atomImg.classList.add("rotate")
+})
+
+atomImg.addEventListener("mouseout", () => {
+  atomImg.classList.remove("rotate")
 })
